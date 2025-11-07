d58f8299ff95a2b961346e25893f2ac2919ce7a6
dikshakushwah04 <dikshakushwah484@gmail.com>
2025-11-07 18:46:38 -0500

    Update cmake_install.cmake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,6 +1,4 @@
-# Install script for directory: /Users/dikshakushwah0408icloud.com/Desktop/shortest-path-finder
 
-# Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
   set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
